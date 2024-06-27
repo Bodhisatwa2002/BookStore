7eae64f658f964db0ec5caf19545ca42ebf02ce5
Bodhisatwa Chakraborty <bodhisatwachakraborty2002@gmail.com>
2024-06-27 09:08:06 +0530

    added backend database in the home page

--- Frontend/src/components/Freebook.jsx
+++ Frontend/src/components/Freebook.jsx
@@ -1,45 +1,63 @@
-import React from "react";
-import list from "../../public/list.json";
+import React, { useEffect, useState } from "react";
+
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
+
+import axios from "axios";
+
 import Cards from "./Cards";
 function Freebook() {
-  const filterData = list.filter((data) => data.category === "free");
-   var settings = {
-     dots: true,
-     infinite: false,
-     speed: 500,
-     slidesToShow: 3,
-     slidesToScroll: 3,
-     initialSlide: 0,
-     responsive: [
-       {
-         breakpoint: 1024,
-         settings: {
-           slidesToShow: 3,
-           slidesToScroll: 3,
-           infinite: true,
-           dots: true,
-         },
-       },
-       {
-         breakpoint: 600,
-         settings: {
-           slidesToShow: 2,
-           slidesToScroll: 2,
-           initialSlide: 2,
-         },
-       },
-       {
-         breakpoint: 480,
-         settings: {
-           slidesToShow: 1,
-           slidesToScroll: 1,
-         },
-       },
-     ],
-   };
+  const [book, setBook] = useState([]);
+  useEffect(() => {
+    const getBook = async () => {
+      try {
+        const res = await axios.get("http://localhost:4001/book");
+
+        const data = res.data.filter((data) => data.category === "free");
+        console.log(data);
+        setBook(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getBook();
+  }, []);
+
+  var settings = {
+    dots: true,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    initialSlide: 0,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
   return (
     <>
       <div className=" max-w-screen-2xl container mx-auto md:px-20 px-4">
@@ -51,9 +69,10 @@ function Freebook() {
             corporis nulla non suscipit, iure neque earum?
           </p>
         </div>
+
         <div>
           <Slider {...settings}>
-            {filterData.map((item) => (
+            {book.map((item) => (
               <Cards item={item} key={item.id} />
             ))}
           </Slider>
@@ -62,5 +81,4 @@ function Freebook() {
     </>
   );
 }
-
 export default Freebook;
